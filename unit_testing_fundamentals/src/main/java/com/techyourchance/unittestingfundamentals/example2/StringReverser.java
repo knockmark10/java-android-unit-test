97607cedd0b8116987cd4a7784587965ce613af8
Marco Chavez <mchaveza@ia.com.mx>
2020-06-11 07:58:29 -0500

    Feat(UnitTestingFundamentals): StringReverser test
    StringReverserTest implementation as example.

--- unit_testing_fundamentals/src/main/java/com/techyourchance/unittestingfundamentals/example2/StringReverser.java
+++ unit_testing_fundamentals/src/main/java/com/techyourchance/unittestingfundamentals/example2/StringReverser.java
@@ -1,10 +1,12 @@
 package com.techyourchance.unittestingfundamentals.example2;
 
+import com.sun.istack.internal.Nullable;
+
 public class StringReverser {
 
     public String reverse(String string) {
         StringBuilder sb = new StringBuilder();
-        for (int i = string.length() ; i > 0; i--) {
+        for (int i = string.length() - 1; i >= 0; i--) {
             sb.append(string.charAt(i));
         }
         return sb.toString();
--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/example2/StringReverserTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/example2/StringReverserTest.java
@@ -1,6 +1,10 @@
 package com.techyourchance.unittestingfundamentals.example2;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class StringReverserTest {
 
@@ -9,6 +13,24 @@ public class StringReverserTest {
     @Before
     public void setup() throws Exception {
         SUT = new StringReverser();
+    }
+
+    @Test
+    public void reverse_emptyString_emptyStringReturned() {
+        String result = this.SUT.reverse("");
+        assertThat(result, is(""));
+    }
+
+    @Test
+    public void reverse_oneWordString_reversedWordReturned() {
+        String result = this.SUT.reverse("Marco");
+        assertThat(result, is("ocraM"));
+    }
+
+    @Test
+    public void reverse_longString_sameStringReturned() {
+        String result = this.SUT.reverse("Marco Chavez");
+        assertThat(result, is("zevahC ocraM"));
     }
 
 }
