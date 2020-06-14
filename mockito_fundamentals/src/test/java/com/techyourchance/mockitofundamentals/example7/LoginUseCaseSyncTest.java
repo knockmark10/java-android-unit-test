4d3c2b10cdf9533cd4fbe00bd755ad90a3988368
Marco Chavez <mchaveza@ia.com.mx>
2020-06-14 13:19:21 -0500

    Feat(MockitoFundamentals): Template for class to show as example
    LoginUseCaseSyncTest from MockitoFundamentals is not an empty shell.

--- mockito_fundamentals/src/test/java/com/techyourchance/mockitofundamentals/example7/LoginUseCaseSyncTest.java
+++ mockito_fundamentals/src/test/java/com/techyourchance/mockitofundamentals/example7/LoginUseCaseSyncTest.java
@@ -1,6 +1,84 @@
 package com.techyourchance.mockitofundamentals.example7;
 
+import org.junit.Test;
+
 public class LoginUseCaseSyncTest {
 
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+    public static final String AUTH_TOKEN = "authToken";
+
+    private LoginUseCaseSync SUT;
+
+
+    @Test
+    public void loginSync_success_usernameAndPasswordPassedToEndpoint() throws Exception {
+
+    }
+
+    @Test
+    public void loginSync_success_authTokenCached() throws Exception {
+
+    }
+
+    @Test
+    public void loginSync_generalError_authTokenNotCached() throws Exception {
+
+    }
+
+    @Test
+    public void loginSync_authError_authTokenNotCached() throws Exception {
+
+    }
+
+    @Test
+    public void loginSync_serverError_authTokenNotCached() throws Exception {
+
+    }
+
+    @Test
+    public void loginSync_success_loggedInEventPosted() throws Exception {
+
+    }
+
+    @Test
+    public void loginSync_generalError_noInteractionWithEventBusPoster() throws Exception {
+
+    }
+
+    @Test
+    public void loginSync_authError_noInteractionWithEventBusPoster() throws Exception {
+
+    }
+
+    @Test
+    public void loginSync_serverError_noInteractionWithEventBusPoster() throws Exception {
+
+    }
+
+    @Test
+    public void loginSync_success_successReturned() throws Exception {
+
+    }
+
+    @Test
+    public void loginSync_serverError_failureReturned() throws Exception {
+
+    }
+
+    @Test
+    public void loginSync_authError_failureReturned() throws Exception {
+
+    }
+
+    @Test
+    public void loginSync_generalError_failureReturned() throws Exception {
+
+    }
+
+    @Test
+    public void loginSync_networkError_networkErrorReturned() throws Exception {
+
+    }
 
 }
